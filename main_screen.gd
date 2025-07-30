9d8115aa4dcee2b97cf8dc64c851c6d630694ec7
Mustitz <mustitz@gmail.com>
2025-07-30 21:21:42 +0300

    Show game grid.

--- main_screen.gd
+++ main_screen.gd
@@ -3,6 +3,9 @@ extends Control
 func _ready():
 	setup_adaptive_ui()
 
+func _on_new_game_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
 func _on_exit_btn_pressed() -> void:
 	get_tree().quit()
 
