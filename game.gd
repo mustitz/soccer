e4036be310abf3d75bc86569d9fb2194fbc1a00d
Mustitz <mustitz@gmail.com>
2025-07-31 06:02:19 +0300

    Add Navigation Bar to Game.

--- game.gd
+++ game.gd
@@ -8,6 +8,9 @@ func _ready():
 	var mm_to_pixels = get_dpi() / 25.4
 	var cell_size = int(cell_size_in_mm * mm_to_pixels)
 
+	var panel = $VBoxContainer/Panel
+	panel.custom_minimum_size.y = 10 * mm_to_pixels
+
 	board.cell_width = cell_size
 	board.cell_height = cell_size
 	board.update_size()
